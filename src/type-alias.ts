6b2511a55182db108efa1b14c098a5856e2a407f
Chinedu Daniel <danielpreciouschinedu@gmail.com>
2026-09-22 07:51:52 +0100

    type-alias to show the blue print and following the blue print

---
+++ src/type-alias.ts
@@ -0,0 +1,14 @@
+type Product = {
+    name: string;
+    price: number;
+    quantity: number
+};
+
+
+const product: Product = {
+    name: "Laptop",
+    price: 1200,
+    quantity: 5
+};
+
+console.log(product);
